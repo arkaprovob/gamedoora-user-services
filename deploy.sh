b812d3deff81d3c2108f32116a54f8209d23b8a2
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-05-21 01:57:51 +0530

    trial 3

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,12 @@ JAR_FILE=target/*.jar
 REMOTE_DIRECTORY=/home/ec2-user/server/user-services
 REMOTE_JAR_FILE=user-services.jar
 
+# Create a temporary SSH key file
+echo "$SSH_KEY" > /tmp/deploy.key
+chmod 600 /tmp/deploy.key
+
 # connect to the EC2 instance and set up the remote directory
-ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
+ssh -i "/tmp/deploy.key" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
   if [ -d $REMOTE_DIRECTORY ]; then
     cd $REMOTE_DIRECTORY
     if [ -f $REMOTE_JAR_FILE ]; then
@@ -17,14 +21,18 @@ ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
   fi"
 
 # copy the new JAR file to the remote directory and rename it
-scp -i "$SSH_KEY" -o StrictHostKeyChecking=no $JAR_FILE "$SSH_USERNAME@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
+scp -i "/tmp/deploy.key" -o StrictHostKeyChecking=no $JAR_FILE "$SSH_USERNAME@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
 
 # make the new JAR file executable and start the new service
-ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
+ssh -i "/tmp/deploy.key" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
   cd $REMOTE_DIRECTORY
   chmod +x $REMOTE_JAR_FILE
   sudo java -jar -Dserver.port=8081 $REMOTE_JAR_FILE > user_service.log 2> user_service.error.log &"
 
+# Remove the temporary SSH key file
+rm /tmp/deploy.key
+
+# Check the exit status of the previous command and set an output variable accordingly
 if [ $? -eq 0 ]; then
   echo "::set-output name=status::success"
 else
